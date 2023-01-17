7ecd1cfaf5d926fe4227fb07770eb34616b4fee2
Ryan Mansur <64165933+RyanMannsur@users.noreply.github.com>
2023-01-17 09:36:19 -0300

    Delete Phyton/app_phyton directory

--- Phyton/app_phyton/meu_appp.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Digite um Numero: "))
-b = int(input("Digite outro Numero: "))
-soma = a + b
-multiplicacao = a * b
-resto = a % b
-
-print("Soma: {soma} \nMultiplicacao: {mult}\n"
-      "Resto: {resto}".format(soma=soma, resto=resto, mult=multiplicacao))
