02d2cdd79fff11acf3fbe321f8dae72355005974
Ryan Mansur <64165933+RyanMannsur@users.noreply.github.com>
2023-01-17 09:35:33 -0300

    Delete Haskell directory

--- Haskell/transcript.hs
+++
@@ -1,6 +0,0 @@
-transcript :: [Char] -> [Char]
-transcript [] = []
-transcript (a:x) | (a == 'G') = "C"++transcript x
-		| (a == 'C') = "G"++transcript x
-		| (a == 'T') = "A"++transcript x
-		| (a == 'A') = "U"++transcript x
